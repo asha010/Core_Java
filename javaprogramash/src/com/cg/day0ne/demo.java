e96292888b0a382ed24d27d88da8a358a9429d9c
Yevano <Yevano@DESKTOP-T8GTBJB>
2023-10-17 15:54:17 +0530

    first commit

---
+++ javaprogramash/src/com/cg/day0ne/demo.java
@@ -0,0 +1,12 @@
+package com.cg.day0ne;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi");
+		
+
+	}
+
+}
---
+++ javaprogramash/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module javaprogramash {
+}
